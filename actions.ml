9d835e96603df6e2ed7526a96039edccb5b93cec
Jinlyu103 <54463545+Jinlyu103@users.noreply.github.com>
2019-10-23 12:46:29 +0800

    Add files via upload
    这是修改了之后的action.ml文件，在调用print_roles_acts a rolelist时会报错：
    Error: This expression has type
             ('a -> 'b, out_channel, unit, unit, unit, 'a -> 'b)
             CamlinternalFormatBasics.fmt
           but an expression was expected of type
             ('a -> 'b, out_channel, unit, unit, unit, unit)
             CamlinternalFormatBasics.fmt
           Type 'a -> 'b is not compatible with type unit

--- actions.ml
+++ actions.ml
@@ -38,30 +38,32 @@ let compileAct (seq,r1,r2,n,m) roleName =
   else if roleName = r2 then Some (Minus,m)
   else None
 
-let rec output_action_A outc = function
+let rec output_action outc acts rolename = 
+  match acts with
   | `Null -> output_string outc "null"  
-  | `Actlist arr -> print_actlist outc arr
+  | `Actlist arr -> print_actlist outc arr rolename
   | `Act (seq, r1, r2, n, m) ->
-	match compileAct (seq, r1, r2, n, m) "A" with
-    	| Some (Plus, m)  -> printf "+"
-    	| Some (Minus, m) -> printf "-"
+	match compileAct (seq, r1, r2, n, m) rolename with
+    	| Some (Plus, m)  -> printf " (+, m) "
+    	| Some (Minus, m) -> printf " (-, m) "
     	| None -> printf "Empty"
 
-and print_actlist outc arr =
+and print_actlist outc arr rolename =
   output_string outc "Actions{\n";
   List.iteri ~f:(fun i v -> 
   if i > 0 then
      output_string outc "\n-------\n";
-  output_action_A outc v) arr;
+  output_action outc v rolename) arr;
   output_string outc "\n}"
 
-let rec output_action_B outc = function
+let rec output_action_B outc acts = 
+  match acts with
   | `Null -> output_string outc "null"  
   | `Actlist arr -> print_actlist outc arr
   | `Act (seq, r1, r2, n, m) ->
 	match compileAct (seq, r1, r2, n, m) "B" with
-    	| Some (Plus, m)  -> printf "+"
-    	| Some (Minus, m) -> printf "-"
+    	| Some (Plus, m)  -> printf " (+, m)"   
+    	| Some (Minus, m) -> printf " (-, m)"  
     	| None -> printf "Empty"
 
 and print_actlist outc arr =
@@ -71,11 +73,67 @@ and print_actlist outc arr =
      output_string outc "\n-------\n";
   output_action_B outc v) arr;
   output_string outc "\n}"
+
+(*------------------------------------------------*)
+let rec output_action1 outc acts rolename = 
+  match acts with
+  | `Null -> output_string outc "null"  
+  | `Actlist arr -> print_actlist outc arr rolename
+  | `Act (seq, r1, r2, n, m) ->
+	match compileAct (seq, r1, r2, n, m) rolename with
+    	| Some (Plus, m)  -> printf " (+, m) "
+    	| Some (Minus, m) -> printf " (-, m) "
+    	| None -> printf "Empty"
+
+and print_actlist outc arr rolename =
+  output_string outc "Actions{\n";
+  List.iteri ~f:(fun i v -> 
+  if i > 0 then
+     output_string outc "\n-------\n";
+  output_action1 outc v rolename) arr;
+  output_string outc "\n}"
+
+(**print_roles_acts takes three arguments: outc, actlist, rolelist,
+   then print the strand of each role in rolelist *)
+
   
-let output_act outc = function
+(**output_string outc "Actions:\n";
+  List.iter ~f:(fun i rolename -> 
+  if i>0 then
+	output_string outc "Actions:\n";
+  output_action outc acts rolename) rolelist;
+  output_string outc "End\n" 
+  *)
+
+let rec print_roles_acts outc acts rolelist =
+  match acts with
+  | `Null -> output_string outc "null"
+  | `Actlist arr -> print_actlist outc arr rolelist
+  | `Act (seq, r1, r2, n, m) ->
+	let ls = List.map ~f:(fun rolename -> compileAct (seq, r1, r2, n, m) rolename) rolelist in
+  	match ls with
+  	| [] -> printf "Empty"
+  	| l -> List.iter ~f:(fun e -> 
+		match e with
+		| Some (Plus, m) -> printf "+";
+ 		| Some (Minus,m) -> printf "-";
+		| None		-> printf "Empty";) ls;
+
+and print_actlist outc arr rolelist =
+  output_string outc "Actions{\n";
+  List.iteri ~f:(fun i v -> 
+  if i > 0 then
+     output_string outc "\n-------\n";
+  print_roles_acts outc v rolelist) arr;
+  output_string outc "\n}"
+
+let output_act outc value = 
+  let rolelist = ["A";"B"] in
+  match value with
   | `Null  	-> output_string outc "null"
-  | `Actlist arr as a  -> printf "------\nA:\n%a\n-------\nB:\n%a\nEND" output_action_A a output_action_B a
-  | `Act (seq,r1,r2,n,m) as a -> printf "------\nA:\n%a\n-------\nB:\n%a\nEND" output_action_A a output_action_B a
+  | `Actlist arr as a -> printf "--------\n%a\n" print_roles_acts a rolelist;
+  | `Act (seq,r1,r2,n,m) as a -> printf	"--------\n%a\n" print_roles_acts a rolelist;			 
+
 
 
 
